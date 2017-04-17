3557a00e80c7d65834322be2f23bdc7bfab92054
takumid <jordand@andrew.cmu.edu>
2017-04-16 21:05:22 -0400

    Done, for now
    Will add resume and portfolio buttons w/ PDF accessibility later

--- engine9/script.js
+++ engine9/script.js
@@ -18,4 +18,4 @@ function ws_rotate(i,h,a){var d=jQuery;var g=d(this);var e=d(".ws_list",a);var b
 // Obfuscated by Javascript Obfuscator
 // http://javascript-source.com
 //***********************************************
-jQuery("#wowslider-container1").wowSlider({effect:"rotate",prev:"",next:"",duration:20*100,delay:40*100,width:960,height:300,autoPlay:true,autoPlayVideo:false,playPause:true,stopOnHover:false,loop:false,bullets:1,caption:true,captionEffect:"parallax",controls:true,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
+jQuery("#wowslider-container9").wowSlider({effect:"rotate",prev:"",next:"",duration:20*100,delay:40*100,width:960,height:300,autoPlay:true,autoPlayVideo:false,playPause:true,stopOnHover:false,loop:false,bullets:1,caption:true,captionEffect:"parallax",controls:true,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
